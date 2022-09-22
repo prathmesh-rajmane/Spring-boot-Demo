bb9d2d29d8de390d766ed6d82863c54a056e2602
prathmesh-rajmane <109971130+prathmesh-rajmane@users.noreply.github.com>
2022-09-22 18:21:43 +0530

    added all files

---
+++ src/main/java/com/prathmesh/demo/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.prathmesh.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class WelcomeController {
+
+        @GetMapping("/welcome")
+        public String welcome() {
+
+                return "Welcome to my first spring boot page";
+
+        }
+}
+
